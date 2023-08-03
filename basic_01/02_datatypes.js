ab2d875cc928f07d88592afab3574e20f314cea3
shreyasvekariya <140139916+shreyasvekariya@users.noreply.github.com>
2023-08-03 12:30:31 +0000

    datatypes

---
+++ basic_01/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // trat all js code as newer version
+
+// alert(3+3) // we arre using nodejs, not browser
+
+console.log(3
+    +
+    3)//code readability shoukd be high 
+
+console.log("shrey")
+
+    let name = "shrey"
+    let age  = "28"
+    let isloggedin = "false"
+    let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/falsse
+// null =>stndlone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // undefined
+
+
