a2d70e9fa067fd96a2c15650461b9a29f8a4bcef
shreyasvekariya <140139916+shreyasvekariya@users.noreply.github.com>
2023-08-04 15:38:17 +0000

    conversition

---
+++ basic_01/03_conversition and confusition.js
@@ -0,0 +1,25 @@
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber );
+console.log( valueInNumber );
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoogedIn = 1
+
+let booleanIsLoogedIn = Boolean(isLoogedIn)
+console.log(booleanIsLoogedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shrey" => true
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
